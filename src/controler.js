53d0115f53ac7c5c1731459a3d678eb1904b38a2
DashaKovshikova <70746185+DashaKovshikova@users.noreply.github.com>
2020-11-30 16:26:50 +0500

    Add files via upload

---
+++ src/controler.js
@@ -0,0 +1,114 @@
+export default class Controller {
+    constructor(game, view) {
+        this.game = game;
+        this.view = view;
+        this.isPlaying = false;
+        this.intervalId = null;
+
+
+
+        document.addEventListener('keydown', this.handelKeyDown.bind(this));
+        document.addEventListener('keyup', this.handelKeyUp.bind(this));
+        this.view.renderStartScreen();
+    }
+
+    update() {
+        this.game.movePieceDown();
+        this.updateView();
+    }
+
+    play() {
+        this.isPlaying = true;
+        this.startTimer();
+        this.updateView();
+    }
+    pause() {
+        this.isPlaying = false;
+        this.stopTimer();
+        this.updateView();
+    }
+
+    reset() {
+        this.game.reset();
+        this.play();
+    }
+
+    updateView() {
+        const state = this.game.getState();
+        if (state.isGameOver) {
+            this.view.renderEndScreen(state);
+        } else if (this.isPlaying) {
+            this.view.renderMainScreen(state);
+        } else {
+            this.view.renderPausetScreen();
+        }
+    }
+
+    startTimer() {
+        const speed = 1000 - this.game.getState().level * 100;
+        if (!this.intervalId) {
+            this.intervalId = setInterval(() => {
+                    this.update();
+                },
+                speed > 0 ? speed : 100);
+        }
+
+    }
+
+
+    stopTimer() {
+
+        if (this.intervalId) {
+            clearInterval(this.intervalId);
+            this.intervalId = null;
+        }
+    }
+
+    handelKeyDown(event) {
+        const state = this.game.getState();
+
+        switch (event.keyCode) {
+            case 13:
+                if (state.isGameOver) {
+                    this.reset();
+                } else if (this.isPlaying) {
+                    this.pause();
+                } else {
+                    this.play();
+                }
+                break;
+
+            case 37:
+                game.movePieceLeft();
+                this.updateView();
+                break;
+
+            case 38:
+                game.rotatePiece();
+                this.updateView();
+                break;
+            case 39:
+                game.movePieceRight();
+                this.updateView();
+                break;
+            case 40:
+                this.stopTimer();
+                game.movePieceDown();
+                this.updateView();
+                break;
+        }
+    }
+
+    handelKeyUp(event) {
+        switch (event.keyCode) {
+
+            case 40:
+                this.startTimer();
+                break;
+        }
+    }
+
+
+}
+
+//View.renderMainScreen(game.getState());
